c6f5bdf53e54fb290cde7acc9153d3338be3ff9e
Andy-Castle <masterchief_and@live.com>
2025-04-23 22:45:28 -0500

    Añadir estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import MyButton from "./components/Button";
 import AboutPage from "./components/About_Page";
+import Styles from "./components/Styles";
 
 function App() {
   return (
     <>
+      <Styles />
+
       <div>
         <h1>Bienvenido a mi aplicación</h1>
         <MyButton />
---
+++ src/components/Styles.jsx
@@ -0,0 +1,5 @@
+import "../styles/Styles.css";
+
+export default function Styles() {
+  return <img src="public\vite.svg" alt="Logo de vite" className="avatar" />;
+}
